a9b83c3147452e090a0bef8f4c434015ffb21ea6
Lucky13 <luckyboy13@mail.ru>
2018-03-24 00:47:10 +0300

    Доработал решение 5 задачи 4 недели.

--- mmnosovskiy/week-4/task-5/main.cpp
+++ mmnosovskiy/week-4/task-5/main.cpp
@@ -6,6 +6,27 @@
 #include <fstream>
 #include <cmath>
 
+double f(double x, double c, double k, double m)
+{
+    return 49 / 2. * (x * x) + x * (7 * c - 5 * k) - m;
+}
+
+double findRoot(double c1, double k, double m)
+{
+    int n = 0;
+    double a = -(7 * c1 - 5 * k) / 2 / 49 * 2., b = 1000000000, c, eps = 0.01;
+    do
+    {
+        c = (a + b) / 2;
+        if (f(c, c1, k, m) * f(a, c1, k, m) <= 0) b = c;
+        else a = c;
+
+        n += 1;
+
+    } while (fabs(a - b) >= eps);
+    return c;
+}
+
 int main()
 {
     std::ifstream fin("input.txt", std::ios::in);
@@ -36,13 +57,9 @@ int main()
         fout << i - 2;
     else
     {
-        double t = (7 * i - 5 * k) / 2.;
-        double t2 = t * t;
-        double D = t2 + 49 * rest;
-        n = (-(7 * i - 5 * k) / 2. + std::sqrt(D)) / 49 / 2;
+        n = findRoot(i, k, m);
         auto weeks = (unsigned long long) n;
-        rest += weeks * 5 * k;
-        rest -= weeks * 7 * (2 * i + weeks * 7) / 2;
+        rest += (weeks * 5 * k) - (weeks * 7 * (2 * i + weeks * 7) / 2);
         i += weeks * 7;
         while (rest >= 0)
         {
