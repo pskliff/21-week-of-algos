57c325c0f04d17821969793a61cdcdf5db3c1827
pskliff <bronkout@yahoo.com>
2018-05-27 10:12:37 +0300

    что-то менял в 3й неделе

--- pskliff/week3/task3rects.cpp
+++ pskliff/week3/task3rects.cpp
@@ -1,47 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
-
-using namespace std;
-
-int main()
-{
-    const int n = 8;
-//    pair<int, int> points[n] = {make_pair(1,1), make_pair(1,3), make_pair(1,4), make_pair(2,4), make_pair(3,1),
-//                                make_pair(3,3), make_pair(3,4), make_pair(4,1), make_pair(4,4), make_pair(5,2)}; // 5
-
-    pair<int, int> points[n] = {make_pair(-2,4), make_pair(4,10), make_pair(7,7), make_pair(1,1),
-                                make_pair(3,3), make_pair(5,1), make_pair(5,5), make_pair(7,3),
-                                make_pair(-2,0), make_pair(-5,0), make_pair(-5,4), make_pair(-2,6), make_pair(-5,6),
-                                make_pair(-8,0), make_pair(-8,6)}; // 7
-
+//#include <iostream>
+//#include <vector>
+//#include <unordered_set>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    const int n = 8;
+////    pair<int, int> points[n] = {make_pair(1,1), make_pair(1,3), make_pair(1,4), make_pair(2,4), make_pair(3,1),
+////                                make_pair(3,3), make_pair(3,4), make_pair(4,1), make_pair(4,4), make_pair(5,2)}; // 5
+//
 //    pair<int, int> points[n] = {make_pair(-2,4), make_pair(4,10), make_pair(7,7), make_pair(1,1),
-//                                make_pair(3,3), make_pair(5,1), make_pair(5,5), make_pair(7,3)}; // 2
-
-    int count = 0;
-    unordered_map<int, unordered_set<int>> hash_points;
-    for (auto el : points)
-        hash_points[el.first].insert(el.second);
-
-    for (int i = 0; i < n - 2; ++i)
-    {
-        for (int j = i + 1; j < n - 1; ++j)
-        {
-            for (int k = j + 1; k < n; ++k)
-            {
-                int x1 = points[i].first, y1 = points[i].second;
-                int dx1 = x1 - points[j].first, dy1 = y1 - points[j].second;
-                int dx2 = points[k].first - points[j].first, dy2 = points[k].second - points[j].second;
-                if (dx1 * dx2 + dy1 * dy2 != 0)
-                    continue;
-                int x_new = x1 + dx2, y_new = y1 + dy2;
-                if (hash_points.find(x_new) != hash_points.end() && hash_points[x_new].find(y_new) != hash_points[x_new].end())
-                    ++count;
-            }
-        }
-    }
-
-    cout << count / 2;
-    return 0;
-}
+//                                make_pair(3,3), make_pair(5,1), make_pair(5,5), make_pair(7,3),
+//                                make_pair(-2,0), make_pair(-5,0), make_pair(-5,4), make_pair(-2,6), make_pair(-5,6),
+//                                make_pair(-8,0), make_pair(-8,6)}; // 7
+//
+////    pair<int, int> points[n] = {make_pair(-2,4), make_pair(4,10), make_pair(7,7), make_pair(1,1),
+////                                make_pair(3,3), make_pair(5,1), make_pair(5,5), make_pair(7,3)}; // 2
+//
+//    int count = 0;
+//    unordered_map<int, unordered_set<int>> hash_points;
+//    for (auto el : points)
+//        hash_points[el.first].insert(el.second);
+//
+//    for (int i = 0; i < n - 2; ++i)
+//    {
+//        for (int j = i + 1; j < n - 1; ++j)
+//        {
+//            for (int k = j + 1; k < n; ++k)
+//            {
+//                int x1 = points[i].first, y1 = points[i].second;
+//                int dx1 = x1 - points[j].first, dy1 = y1 - points[j].second;
+//                int dx2 = points[k].first - points[j].first, dy2 = points[k].second - points[j].second;
+//                if (dx1 * dx2 + dy1 * dy2 != 0)
+//                    continue;
+//                int x_new = x1 + dx2, y_new = y1 + dy2;
+//                if (hash_points.find(x_new) != hash_points.end() && hash_points[x_new].find(y_new) != hash_points[x_new].end())
+//                    ++count;
+//            }
+//        }
+//    }
+//
+//    cout << count / 2;
+//    return 0;
+//}
--- pskliff/week3/task4strings.cpp
+++ pskliff/week3/task4strings.cpp
@@ -1,20 +1,32 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//    string s1 = "aaaaaa", s2 = "bbbbbb", res = "";
-//    bool was[256] = {false};
-//
-//    for (int i = 0; i < s2.length(); ++i)
-//        was[s2[i] + 128] = true;
-//
-//    for (int i = 0; i < s1.length(); ++i)
-//        if(!was[s1[i] + 128])
-//            res += s1[i];
-//
-//    cout << (res == "" ? "-1" : res);
-//    return 0;
-//}
-//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "aaaaaa", s2 = " !01BbZz", res = "";
+    bool was[256] = {false};
+
+    for (int i = 0; i < s2.length(); ++i)
+    {
+        cout << (int)s2[i] << endl;
+        cout << ((int)s2[i] + 128) << endl;
+        was[s2[i] + 128] = true;
+    }
+
+
+    for (int i = 0; i < s1.length(); ++i)
+        if(!was[s1[i] + 128])
+            res += s1[i];
+
+    cout << (res == "" ? "-1" : res);
+    return 0;
+}
+
+
+//  void moveZeroes(vector<int>& nums) {
+//         for(int curr = 0, lst = 0; curr < nums.size(); ++curr)
+//             if(nums[curr])
+//                 swap(nums[lst++], nums[curr]);
+//     }
+
